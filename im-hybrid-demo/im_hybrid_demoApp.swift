c4f2466f777994e7c659bf53a7549268c0cd4f4e
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2024-11-19 14:37:26 +1300

    Initial Commit

---
+++ im-hybrid-demo/im_hybrid_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  im_hybrid_demoApp.swift
+//  im-hybrid-demo
+//
+//  Created by Personal on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct im_hybrid_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
